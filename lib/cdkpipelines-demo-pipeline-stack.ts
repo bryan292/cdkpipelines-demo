67a2d0ea6d8b687759061e4d6106309ca110943d
Bryan Cerdas <bryan.cerdas@BRYANS-MACBOOK-PRO.local>
2021-09-29 19:20:50 -0600

    teste

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -30,21 +30,3 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
     // ...
   }
 }
-The preceding code defines the following basic properties of the pipeline:
-
-Its name.
-Where to find the source. You need to change that part to match the name of your own GitHub repo.
-How to do the build and synthesis. For this use case, you use a standard NPM build (this type of build runs npm run build followed by npx cdk synth).
-You also need to instantiate CdkpipelinesDemoPipelineStack with the account and Region where you want to deploy the pipeline. Put the following code in bin/cdkpipelines-demo.ts (be sure to replace ACCOUNT1 and the Region in there if necessary):
-
-#!/usr/bin/env node
-import { App } from '@aws-cdk/core';
-import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
-
-const app = new App();
-
-new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
-  env: { account: 'ACCOUNT1', region: 'us-east-2' },
-});
-
-app.synth();
