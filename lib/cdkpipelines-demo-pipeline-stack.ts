b71b56f404f9b4743200b0cf6dee9001e44b3b2a
Bryan Cerdas <bryan.cerdas@BRYANS-MACBOOK-PRO.local>
2021-10-03 12:02:53 -0600

    workking with codepipelineç

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -3,6 +3,9 @@ import { CdkPipeline, CodePipeline, CodePipelineSource, ShellStep, SimpleSynthAc
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as codecommit from '@aws-cdk/aws-codecommit';
+
 /**
  * The stack that defines the application pipeline
  */
@@ -33,5 +36,30 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
     });
 
 
+    const gitHubSource = codebuild.Source.gitHub({
+      owner: 'bryan292',
+      repo: 'react-calculator',
+      webhook: true, // optional, default: true if `webhookFilters` were provided, false otherwise
+      webhookTriggersBatchBuild: true, // optional, default is false
+     // webhookFilters: [
+     //   codebuild.FilterGroup
+      //    .inEventOf(codebuild.EventAction.PUSH)
+     //     .andBranchIs('master')
+     // ], // optional, by default all pushes and Pull Requests will trigger a build
+     
+    });
+
+    const code = new codebuild.Project(this,'CodeCommit',{
+      environment:{
+        buildImage: codebuild.LinuxBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux2-x86_64-standard:3.0'),
+        
+      },
+      source: gitHubSource,
+
+    })
+
+
     }
+
+
   }
