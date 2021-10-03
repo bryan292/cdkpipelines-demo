b0c94a7e7c3dd557feaf0a5a1d7e4f4e964a301c
Bryan Cerdas <bryan.cerdas@BRYANS-MACBOOK-PRO.local>
2021-10-03 12:13:13 -0600

    adding a stage

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -5,6 +5,7 @@ import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 
 import * as codebuild from '@aws-cdk/aws-codebuild';
 import * as codecommit from '@aws-cdk/aws-codecommit';
+import { CdkpipelinesDemoStage } from './cdkpipelines-demo-stage';
 
 /**
  * The stack that defines the application pipeline
@@ -35,29 +36,9 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
       }),
     });
 
-
-    const gitHubSource = codebuild.Source.gitHub({
-      owner: 'bryan292',
-      repo: 'react-calculator',
-      webhook: true, // optional, default: true if `webhookFilters` were provided, false otherwise
-      webhookTriggersBatchBuild: true, // optional, default is false
-     // webhookFilters: [
-     //   codebuild.FilterGroup
-      //    .inEventOf(codebuild.EventAction.PUSH)
-     //     .andBranchIs('master')
-     // ], // optional, by default all pushes and Pull Requests will trigger a build
-     
-    });
-
-    const code = new codebuild.Project(this,'CodeCommit',{
-      environment:{
-        buildImage: codebuild.LinuxBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux2-x86_64-standard:3.0'),
-        
-      },
-      source: gitHubSource,
-
-    })
-
+    pipeline.addApplicationStage(new CdkpipelinesDemoStage(this,'PrePROD',{
+      env: { account: '389938679709', region: 'us-east-2' },
+    }))
 
     }
 
