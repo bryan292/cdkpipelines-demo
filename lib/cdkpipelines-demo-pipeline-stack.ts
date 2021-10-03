de5d3ee6f4d37b4a31382862ab99952b73d8d8c9
Bryan Cerdas <bryan.cerdas@BRYANS-MACBOOK-PRO.local>
2021-10-03 10:29:12 -0600

    intial

---
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -0,0 +1,37 @@
+import { Construct, SecretValue, Stack, StackProps, } from '@aws-cdk/core';
+import { CdkPipeline, CodePipeline, CodePipelineSource, ShellStep, SimpleSynthAction } from "@aws-cdk/pipelines";
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+
+/**
+ * The stack that defines the application pipeline
+ */
+export class CdkpipelinesDemoPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipeline = new CdkPipeline(this, 'Pipeline', {
+      // The pipeline name
+      pipelineName: 'MyServicePipeline',
+      cloudAssemblyArtifact,
+      sourceAction: new codepipeline_actions.GitHubSourceAction({
+        actionName: 'Github',
+        output: sourceArtifact,
+        oauthToken: SecretValue.secretsManager('github-token'),
+        owner: 'bryan292',
+        repo: 'cdkpipelines-demo',
+        branch: 'master'
+      }),
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+        buildCommand: 'npm run build'
+      }),
+    });
+
+
+    }
+  }
